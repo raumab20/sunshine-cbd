75846719c3a1c3522973bfd68bedc1d5ec1b7fa7
Marc Raunigg <raumab20@htl-kaindorf.at>
2024-10-05 16:48:05 +0200

    Removed babel

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    presets: [
-      '@babel/preset-env',        // Für moderne JavaScript-Funktionen
-      '@babel/preset-react',      // Für JSX
-      '@babel/preset-typescript', // Für TypeScript (falls verwendet)
-    ],
-  };
-  
