ca2a8e43c854c246b8a17f49feb52af73de3f1d8
Marc Raunigg <raumab20@htl-kaindorf.at>
2024-11-05 15:24:00 +0100

    Test

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -64,7 +64,7 @@ export default async function ProductPage({
       });
 
       if (!res.ok) {
-        throw new Error("Fehler beim Hinzufügen zum Warenkorb");
+        throw new Error("Fehler beim Hinzufügen zum Warenkorb!");
       }
 
       const data = await res.json();
