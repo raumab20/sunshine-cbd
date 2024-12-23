4882cba06916133ded99cd5ca9b9de4910a5c9fc
Marc Raunigg <raumab20@htl-kaindorf.at>
2024-12-23 19:43:50 +0100

    Added default timeout values

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Setup code für Node Events
     },
-    baseUrl: "http://localhost:45620",
+    baseUrl: 'http://localhost:45620', // Beispiel URL, falls benötigt
+    defaultCommandTimeout: 10000, // Erhöht den Timeout für jeden Befehl
+    pageLoadTimeout: 60000, // Erhöht den Timeout für das Laden einer Seite
+    requestTimeout: 10000, // Timeout für Netzwerkanfragen
+    retries: {
+      runMode: 2, // Anzahl der Versuche im 'run' Modus
+      openMode: 0, // Anzahl der Versuche im 'open' Modus
+    },
+    screenshotsFolder: 'cypress/screenshots', // Speicherort für Screenshots
+    videosFolder: 'cypress/videos', // Speicherort für Videos
   },
 });
