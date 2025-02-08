8f60f8784fb449f99f702637f150942bd4cb9fb2
Marc Raunigg <raumab20@htl-kaindorf.at>
2025-02-08 12:59:56 +0100

    giving more memory

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,17 @@ const nextConfig = {
   typescript: {
     // Ignoriert TypeScript-Fehler während des Builds
     ignoreBuildErrors: true,
-  }
+  },
+  eslint: {
+    // Ignoriert ESLint-Fehler während des Builds
+    ignoreDuringBuilds: true,
+  },
+  productionBrowserSourceMaps: false, // Deaktiviert Source Maps für geringeren Speicherverbrauch
+  experimental: {
+    workerThreads: false, // Deaktiviert Worker Threads zur Reduzierung des RAM-Verbrauchs
+    cpus: 1,              // Limitiert die Anzahl der verwendeten CPU-Kerne
+  },
+  output: 'standalone',    // Optimiert den Build für eine schlankere, standalone Ausführung
 };
 
 export default nextConfig;
